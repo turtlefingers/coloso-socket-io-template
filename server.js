a9f3781c77274f1c29c862b4f9fcc97172f77bca
Glitch (glitch-hello-node) <none>
2021-11-08 01:49:58 +0000

    😘👌 Checkpoint ./package.json:38467679/5323 ./server.js:38467679/5883 ./index.html:38467679/15

--- server.js
+++ server.js
@@ -1,123 +1,67 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
+import express from 'express';
+import { Server } from 'http';
+import socketio from 'socket.io';
+import path from 'path';
 
-const path = require("path");
+const __dirname = path.resolve();
+const app = express();
+const http = Server(app);
+const io = socketio(http);
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false
+
+/*=== 원하는 path에 html 파일을 연결 ===*/
+
+app.get('/', function(req, res) {
+    let _url = req.url;
+    res.sendFile(__dirname + '/index.html');
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+
+app.use("/", express.static(__dirname + "/"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
+// 소켓 연결 시
+io.on('connection', function(socket) {
 
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
+    console.log("user connected");
 
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
-});
+    let onevent = socket.onevent;
+    socket.onevent = function(packet) {
+        let args = packet.data || [];
+        onevent.call(this, packet); // original call
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
+        // 패킷 데이터가 object 가 아니라 string 으로 들어왔을 시 변환
+        if(packet.data[1] && typeof packet.data[1] == "string"){
+            console.log("packet.data", packet.data);
+            packet.data[1] = JSON.parse(packet.data[1]);
+        }
 
-/**
-* Our home page route
-*
-* Returns src/pages/index.hbs with data built into it
-*/
-fastify.get("/", function(request, reply) {
-  
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
+        // 아래 모든이벤트 일괄적용을 위한 코드
+        packet.data = ["*"].concat(args);
+
+        onevent.call(this, packet); // additional call to catch-all
     };
-  }
-  
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  reply.view("/src/pages/index.hbs", params);
+
+    // 모든 이벤트 일괄 적용
+    socket.on('*', function(event, data){
+
+    });
+
+    /* 테스트 페이지 */
+    socket.on('SendTest', function(data) {
+        let text = "";
+        for(let prop in data){
+            if(text != "") text += ", ";
+            text += "["+prop + "] " + data[prop];
+        }
+        // Admin 페이지에 메세지를 전달함
+        socket.emit("message", { text: "서버로 전송된 데이터\n" + text });
+    });
 });
 
-/**
-* Our POST route to handle and react to form submissions 
-*
-* Accepts body data indicating the user choice
-*/
-fastify.post("/", function(request, reply) {
-  
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-  
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-  
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-    
-    // Load our color data file
-    const colors = require("./src/colors.json");
-    
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
-      
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
-  
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  reply.view("/src/pages/index.hbs", params);
-});
 
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
+/*=== 서버 시작 ===*/
+http.listen(80, function() {
+    console.log('listening on *:80');
 });
