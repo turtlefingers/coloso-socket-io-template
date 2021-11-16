4e580e0f219c22dd541d103353f7bc2f392dee00
Glitch (glitch-hello-node) <none>
2021-11-16 15:05:40 +0000

    🌂🐾 Checkpoint ./index.html:38467679/1320 ./visual/scalar/sketch.js:38467679/394

--- visual/scalar/sketch.js
+++ visual/scalar/sketch.js
@@ -13,34 +13,38 @@ function windowResized(){
 
 function draw(){
   theta += v1;
-  let move1 = sin(theta) * 30;
-  let move2 = sin(theta + PI*0.5) * 30;
-  let move3 = sin(theta + PI*1) * 30;
-  let move4 = sin(theta + PI*1.5) * 30;
+  let move1 = sin(theta);
+  let move2 = sin(theta + PI*0.5);
+  let move3 = sin(theta + PI*1)
+  let move4 = sin(theta + PI*1.5);
   
+  // 사각형 위치기준을 중앙으로
   rectMode(CENTER);
   
+  
+  // 가운데로 중심축 이동
   translate(width/2, height/2);
   
   // 배경 검은색 + 반투명
   background(0);
   
   // 왼다리
-  rect(-20,80+move2,30,50);
+  rect(-20,80+move2*30,30,50);
   
   // 오른다리
-  rect(20,80+move4,30,50);
+  rect(20,80+move4*30,30,50);
   
   // 몸
-  ellipse(0,move1*0.8,150,150);
+  ellipse(0,move1*24,150,150);
   
   // 머리
-  ellipse(0,-100 + move1*0.5,50,50);
+  ellipse(0,-100 + move1*15,70,70);
   
-  // 머리
-  ellipse(-50,0 + move4,30,30);
+  // 왼팔
+  ellipse(-50,0 + move4*30,30,30);
   
-  ellipse(50,0 + move2,30,30);
+  // 오른팔
+  ellipse(50,0 + move2*30,30,30);
   
 }
 
